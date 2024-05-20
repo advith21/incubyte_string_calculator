216be63c255c02ac6d6be6e4c57f2bce6adda05f
Advith <atm8344@gmail.com>
2024-05-20 20:32:56 +0530

    spec for single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,8 +3,12 @@ require_relative '../calculator/string_calculator.rb'
 RSpec.describe StringCalculator do
     describe "add" do
         context "valid calculations" do
-            it 'should return 0 for a empty string' do
+            it 'Should return 0 for a empty string' do
                 expect(described_class.add("")).to eq(0)
+            end
+
+            it "Should return the same number if string contains single number" do
+                expect(described_class.add("1")).to eq(1)
             end
         end
     end
