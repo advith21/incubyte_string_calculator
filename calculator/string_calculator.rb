972db262919c0ad88c2ba4b53a4a644466298daf
Advith <atm8344@gmail.com>
2024-05-20 20:39:49 +0530

    handling dual numbers

--- calculator/string_calculator.rb
+++ calculator/string_calculator.rb
@@ -3,5 +3,11 @@ class StringCalculator
     return 0 if string_number.empty?
 
     return string_number.to_i if string_number.length == 1
+
+    sum = 0
+    string_number.split(',').each do |number|
+        sum += number.to_i
+    end
+    sum
   end
 end
