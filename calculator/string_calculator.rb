3f43487807ecfd03d44e3d73ce77b6e670ecde03
Advith <atm8344@gmail.com>
2024-05-20 21:23:05 +0530

    Refactoring the number extraction process

--- calculator/string_calculator.rb
+++ calculator/string_calculator.rb
@@ -4,17 +4,22 @@ class StringCalculator
 
     return string_number.to_i if string_number.length == 1
 
-    if string_number.start_with?("//")
-      new_delimiter = string_number.split(/\/\/|\n/)[1]
-      delimiter = "\n|#{new_delimiter}"
-      numbers_list = string_number.split(Regexp.new(delimiter))[2..-1]
-    else 
-      numbers_list = string_number.split(/,(?!\n)|\n/) 
-    end
+    numbers_list = extract_numbers(string_number)
     sum = 0
     numbers_list.each do |number|
       sum += number.to_i
     end
     sum
   end
+
+  private
+    def self.extract_numbers(string_number)
+      if string_number.start_with?("//")
+        new_delimiter = string_number.split(/\/\/|\n/)[1]
+        delimiter = "\n|#{new_delimiter}"
+        string_number.split(Regexp.new(delimiter))[2..-1]
+      else 
+        string_number.split(/,(?!\n)|\n/) 
+      end
+    end
 end
