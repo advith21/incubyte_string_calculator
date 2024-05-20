02325f78c79ab3b022fcd7390aa2fdc345a3789c
Advith <atm8344@gmail.com>
2024-05-20 20:25:57 +0530

    spec for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,1 +1,11 @@
 require_relative '../calculator/string_calculator.rb'
+
+RSpec.describe StringCalculator do
+    describe "add" do
+        context "valid calculations" do
+            it 'should return 0 for a empty string' do
+                expect(described_class.add("")).to eq(0)
+            end
+        end
+    end
+end
