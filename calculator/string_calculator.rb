a2c3f9aa0ecb54bf1ba7ffa3487c9e28dfc960fb
Advith <atm8344@gmail.com>
2024-05-20 21:24:12 +0530

    Refactoring by removing the redundent condition

--- calculator/string_calculator.rb
+++ calculator/string_calculator.rb
@@ -2,8 +2,6 @@ class StringCalculator
   def self.add(string_number)
     return 0 if string_number.empty?
 
-    return string_number.to_i if string_number.length == 1
-
     numbers_list = extract_numbers(string_number)
     sum = 0
     numbers_list.each do |number|
